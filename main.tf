2be25d580dd0a6d74e6bc31c1d9a422853a60de0
sravanprasad-hue <sravanprasad@thestackly.com>
2026-03-16 16:09:57 +0530

    Change EC2 instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Jenkins-Terraform-EC2"
