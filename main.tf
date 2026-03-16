85c6b65da87a927d456d68ff4360f1feff869f54
sravanprasad-hue <sravanprasad@thestackly.com>
2026-03-16 16:21:56 +0530

    Add key_name to aws_instance resource

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.micro"
+  key_name      = "jenkins_server"
 
   tags = {
     Name = "Jenkins-Terraform-EC2"
