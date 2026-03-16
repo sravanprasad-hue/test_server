0a8cd2644b87a629df19619e28cdbdfa98012c0d
sravanprasad-hue <sravanprasad@thestackly.com>
2026-03-16 14:22:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0dee22c13ea7a9a67"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Jenkins-Terraform-Instance"
